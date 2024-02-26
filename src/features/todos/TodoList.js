1f011c07f716a5b5d9a6d0efba7b8850bf9a7a06
israeljoshcaeleb <izael112212@gmail.com>
2024-02-26 13:36:09 +0800

    Implementing simple RTK Query

---
+++ src/features/todos/TodoList.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import { useAddTodoMutation, useDeleteTodoMutation, useGetTodosQuery, useUpdateTodoMutation } from '../api/apiSlice'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
+
+const TodoList = () => {
+    const [newTodo, setNewTodo] = useState('')
+    const {
+        data: todos,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetTodosQuery()
+    const [addTodo] = useAddTodoMutation()
+    const [updateTodo] = useUpdateTodoMutation()
+    const [deleteTodo] = useDeleteTodoMutation()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let idArr = []
+        todos.map(t => idArr.push(Number(t.id)))
+
+        addTodo({
+            userId: 1, id: (Math.max(...idArr) + 1).toString(), title: newTodo,  completed: false})
+        setNewTodo('')
+    }
+
+    const newItemSection = 
+        <form onSubmit={handleSubmit}>
+            <label htmlFor='new-todo'>Enter a new todo</label>
+            <div className='new-todo'>
+                <input 
+                type='text'
+                id='new-todo'
+                value={newTodo}
+                placeholder='Enter new todo'
+                onChange={(e) =>setNewTodo(e.target.value)}
+                />
+            </div>
+            <button className='submit'><FontAwesomeIcon icon={faUpload} /></button>
+        </form>
+    
+    let content;
+    if (isLoading){
+        content = <p>Loading..</p>
+    } else if (isSuccess){
+        content = todos.map(todo => {
+            return (
+                <article key={todo.id}>
+                    <div className='todo'>
+                        <input 
+                        type='checkbox'
+                        checked={todo.completed}
+                        id={todo.id}
+                        onChange={() => updateTodo({ ...todo, completed: !todo.completed})}
+                        />
+                        <label htmlFor={todo.id}>{todo.title}</label>
+                    </div>
+                    <button className='trash' onClick={() => deleteTodo({id: todo.id})}><FontAwesomeIcon icon={faTrash} /></button>
+                </article>
+            )
+        })
+    } else if (isError){
+        content = <p>{error}</p>
+    }
+  return (
+    <main>
+        <h1>Todo List</h1>
+        {newItemSection}
+        {content}
+    </main>
+  )
+}
+
+export default TodoList
